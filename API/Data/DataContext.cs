e9486a925112207d7757054560155ef99fbe6c17
michaelrothbauer93 <michaelrothbauer@gmail.com>
2021-03-05 22:30:34 -0600

    End of Section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,25 +11,41 @@ namespace API.Data
 
         public DbSet<AppUser> Users { get; set;}
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            // Sets the Primary Key to both SourceUserId and LikedUserId
             builder.Entity<UserLike>()
                 .HasKey(k => new {k.SourceUserId, k.LikedUserId});
 
+            // Set up UserLike for liking a user
             builder.Entity<UserLike>()
                 .HasOne(s => s.SourceUser)
                 .WithMany(l => l.LikedUsers)
                 .HasForeignKey(s => s.SourceUserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Set up UserLike for being liked by a user
             builder.Entity<UserLike>()
                 .HasOne(s => s.LikedUser)
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            // Messages should have one recipient (user) and can have many messages received
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Messages should have one sender (user) and can have many messages sent
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
